029b8fa9bfeac18f9e4689ef77aea4485b9c29a4
Michele Mandanici <michimandanici@tim.it>
2026-02-18 16:28:56 +0100

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     setContext(extractedText)
   }
   return (
-    <Container fluid className="py-4 bg">
+    <Container fluid className="min-vh-100 py-4 bg">
       <Row className="justify-content-center">
         <Col xs={12} md={10} lg={6}>
           <header className=" bg2 rounded-3 p-3 mb-4 d-flex align-items-center justify-content-center">
